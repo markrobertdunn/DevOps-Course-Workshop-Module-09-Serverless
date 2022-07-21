6504b4c2c717b32c02d3caf94172bf639cbe66d9
markrobertdunn <mark.dunn@amt-sybex.com>
2022-07-21 13:56:45 +0000

    updated

---
+++ AcmeSubProject/HttpEndpoint/__init__.py
@@ -0,0 +1,20 @@
+import logging
+import time
+import azure.functions as func
+
+
+def main(request: func.HttpRequest) -> func.HttpResponse:
+    logging.info('HTTP trigger function received a request.')
+    start = time.time()
+
+    req_body = request.get_json()
+    subtitle = req_body.get("subtitle")
+
+    time.sleep(5) # Simulating 5 seconds of cpu-intensive processing
+    end = time.time()
+    processingTime = end - start
+
+    return func.HttpResponse(
+        f"Processing took {str(processingTime)} seconds. Translation is: {subtitle}",
+        status_code=200
+    )
